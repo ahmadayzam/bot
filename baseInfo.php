871a615ef57423922a4224f5e6dc9a32ec535ad0
ahmad <39407267+ahmadayzam@users.noreply.github.com>
2025-01-11 18:29:28 +0330

    Add files via upload

---
+++ baseInfo.php
@@ -0,0 +1,9 @@
+<?php
+error_reporting(0);
+$botToken = '7676693411:AAHTXTiRhlLUYmvlx_QosydEMMqJjH1-Gj8';
+$dbUserName = 'l2FbnBRpOMbaw';
+$dbPassword = 'OKG2wmf4wjQNg';
+$dbName = 'wizwiz';
+$botUrl = 'https://wiz.viato.ir/bot/';
+$admin = 1543299428;
+?>
